f392c77740970c3ecbcfe6165df9d9ba6a3d38e6
hoangha <hoangha01627381018@gmail.com>
2024-07-29 23:34:06 +0700

    HoangHa_commit02: update logic be

--- xepGV/src/main/java/com/xepgiangvien2/xepgiangvien2/service/impl/GiangVienServiceImpl.java
+++ xepGV/src/main/java/com/xepgiangvien2/xepgiangvien2/service/impl/GiangVienServiceImpl.java
@@ -3,40 +3,63 @@ package com.xepgiangvien2.xepgiangvien2.service.impl;
 import com.xepgiangvien2.xepgiangvien2.service.GiangVienService;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Service
 public class GiangVienServiceImpl implements GiangVienService {
 
-    @Override
-    public Map<String, List<String>> sapXepGiangVien(List<String> caDay, Map<String, List<String>> giangVien1, List<String> tatCaGiangVien) {
+    private static final List<String> BACKUP_GIANG_VIEN = Arrays.asList( "anhtq51",
+            "vanny4",
+            "thongllh",
+            "lynk18",
+            "phuongck2",
+            "nhatbqm",
+            "hieunm46",
+            "lamtd9",
+            "huongltm",
+            "nhunglt15",
+            "phuongta11",
+            "trangctt4",
+            "phucnh60",
+            "nganntk45",
+            "datvp3",
+            "xuanntt32",
+            "hieuht",
+            "GVDuPhong1",
+            "GVDuPhong2",
+            "GVDuPhong3",
+            "GVDuPhong4",
+            "GVDuPhong5",
+            "GVDuPhong6",
+            "GVDuPhong7",
+            "GVDuPhong8",
+            "GVDuPhong9");
+
+    public Map<String, List<String>> sapXepGiangVien(List<String> caThi, Map<String, List<String>> listGV1, List<String> tatCaGiangVien) {
         Map<String, List<String>> giangVien2 = new HashMap<>();
 
-        for (int i = 0; i < caDay.size(); i++) {
-            String ca = caDay.get(i);
+        for (int i = 0; i < caThi.size(); i++) {
+            String ca = caThi.get(i);
             List<String> giangVienCoSan = new ArrayList<>(tatCaGiangVien);
-            giangVienCoSan.removeAll(giangVien1.get(ca));
+            giangVienCoSan.removeAll(listGV1.get(ca));
 
             List<String> giangVienDuocChon = new ArrayList<>();
             if (i > 0) {
-                String caTruoc = caDay.get(i - 1);
-                for (String giangVien : giangVien1.get(caTruoc)) {
-                    if (giangVienCoSan.contains(giangVien) && giangVienDuocChon.size() < giangVien1.get(ca).size()) {
+                String caTruoc = caThi.get(i - 1);
+                for (String giangVien : listGV1.get(caTruoc)) {
+                    if (giangVienCoSan.contains(giangVien) && giangVienDuocChon.size() < listGV1.get(ca).size()) {
                         giangVienDuocChon.add(giangVien);
                         giangVienCoSan.remove(giangVien);
                     }
                 }
             }
 
-            while (giangVienDuocChon.size() < giangVien1.get(ca).size() && !giangVienCoSan.isEmpty()) {
+            while (giangVienDuocChon.size() < listGV1.get(ca).size() && !giangVienCoSan.isEmpty()) {
                 String giangVienDuocChonThem = giangVienCoSan.remove(0);
                 giangVienDuocChon.add(giangVienDuocChonThem);
             }
 
-            while (giangVienDuocChon.size() < giangVien1.get(ca).size()) {
+            while (giangVienDuocChon.size() < listGV1.get(ca).size()) {
                 giangVienDuocChon.add(null);
                 System.out.println("Không đủ giảng viên có sẵn cho ca " + ca);
             }
@@ -44,6 +67,25 @@ public class GiangVienServiceImpl implements GiangVienService {
             giangVien2.put(ca, giangVienDuocChon);
         }
 
+        // Fill null slots with random backup lecturers
+        fillNullSlots(giangVien2, tatCaGiangVien);
+
         return giangVien2;
+    }
+
+    private void fillNullSlots(Map<String, List<String>> giangVien2, List<String> tatCaGiangVien) {
+        Random random = new Random();
+        for (Map.Entry<String, List<String>> entry : giangVien2.entrySet()) {
+            List<String> giangVienList = entry.getValue();
+            for (int i = 0; i < giangVienList.size(); i++) {
+                if (giangVienList.get(i) == null) {
+                    String backupGiangVien;
+                    do {
+                        backupGiangVien = BACKUP_GIANG_VIEN.get(random.nextInt(BACKUP_GIANG_VIEN.size()));
+                    } while (tatCaGiangVien.contains(backupGiangVien));
+                    giangVienList.set(i, backupGiangVien);
+                }
+            }
+        }
     }
 }
