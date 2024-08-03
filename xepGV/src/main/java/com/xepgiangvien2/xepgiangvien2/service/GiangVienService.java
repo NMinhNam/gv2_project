fd36cd702e2d21a0e0693f086dd231e3dfa48531
Nguyen Minh Nam <minhnam1810@gmai.com>
2024-08-03 11:15:37 +0700

    Commit02_MinhNam: Update API XepLichGiangVien2

--- xepGV/src/main/java/com/xepgiangvien2/xepgiangvien2/service/GiangVienService.java
+++ xepGV/src/main/java/com/xepgiangvien2/xepgiangvien2/service/GiangVienService.java
@@ -1,11 +1,8 @@
 package com.xepgiangvien2.xepgiangvien2.service;
 
-import com.xepgiangvien2.xepgiangvien2.dto.request.ThongTinLichThiRequest;
-import com.xepgiangvien2.xepgiangvien2.dto.response.ThongTinLichThiResponse;
-
 import java.util.List;
 import java.util.Map;
 
 public interface GiangVienService {
-    Map<String, List<String>> sapXepGiangVien(List<String> caDay, Map<String, List<String>> giangVien1, List<String> tatCaGiangVien, List<String> backupGiangVien);
+    Map<String, List<String>> sapXepGiangVien(List<String> caThi, Map<String, List<String>> listGV1, List<String> tatCaGiangVien, List<String> backupGiangVien);
 }
